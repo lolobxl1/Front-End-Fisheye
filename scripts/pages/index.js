355fa0c54c481a503466c10f76fb1f237b9a1417
Loic Guerin <guerin.loic@gmail.com>
2025-10-07 17:15:27 +0200

    GitHub Page test 2

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,7 +2,7 @@ async function getPhotographers() {
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
   const reponse = await fetch(
-    "../../data/photographers.json"
+    "../data/photographers.json"
   );
   data = await reponse.json();
   //console.log(data);
